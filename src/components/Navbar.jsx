49332da89126a6a50a6db8488352e0476e2c1d2c
luckychauhan12w <luckychauhan.12w@gmail.com>
2025-10-30 11:42:44 +0530

    feat: change resume link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,31 @@
-import { FaFilePdf, FaGithub } from 'react-icons/fa';
+import { FaFilePdf, FaGithub } from "react-icons/fa";
 
 const Navbar = () => (
-    <nav className="fixed top-0 left-0 w-full flex items-center justify-between border-b-2 border-black p-5 bg-white z-20">
-        <span className="font-bold text-xl">Lucky Chauhan</span>
-        <ul className="flex gap-4">
-            <li>
-                <a
-                    className="border px-3 py-1 flex items-center gap-2"
-                    href="https://ik.imagekit.io/gaxmekqtag/Resume%20File/resume.pdf"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <FaFilePdf /> Get Resume
-                </a>
-            </li>
-            <li>
-                <a
-                    className="border px-3 py-1 flex items-center gap-2"
-                    href="https://github.com/luckychauhan12w"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <FaGithub /> GitHub
-                </a>
-            </li>
-        </ul>
-    </nav>
+  <nav className="fixed top-0 left-0 w-full flex items-center justify-between border-b-2 border-black p-5 bg-white z-20">
+    <span className="font-bold text-xl">Lucky Chauhan</span>
+    <ul className="flex gap-4">
+      <li>
+        <a
+          className="border px-3 py-1 flex items-center gap-2"
+          href="https://ik.imagekit.io/gaxmekqtag/Resume%20File/Lucky_Resume.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaFilePdf /> Get Resume
+        </a>
+      </li>
+      <li>
+        <a
+          className="border px-3 py-1 flex items-center gap-2"
+          href="https://github.com/luckychauhan12w"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub /> GitHub
+        </a>
+      </li>
+    </ul>
+  </nav>
 );
 
 export default Navbar;
