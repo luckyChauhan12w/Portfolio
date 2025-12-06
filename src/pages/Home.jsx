727fd946e92b56846332ad57f650cdb279d78ef1
Lucky Chauhan <126170989+luckyChauhan12w@users.noreply.github.com>
2025-12-06 11:45:00 +0530

    fix(home): Add dark mode support to Home page
    - Add dark mode background and border colors
    - Add smooth color transitions

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 
 const Home = () => (
-    <section id="home" className="border-2 border-black p-6 overflow-hidden bg-white relative">
+    <section id="home" className="border-2 border-black dark:border-gray-700 p-6 overflow-hidden bg-white dark:bg-gray-900 relative transition-colors duration-200">
         <div className="w-full h-full flex items-center justify-center relative">
             <img
                 src="https://user-images.githubusercontent.com/74038190/225813708-98b745f2-7d22-48cf-9150-083f1b00d6c9.gif"
